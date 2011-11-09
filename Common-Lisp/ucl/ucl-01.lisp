7171e41390c15d4be516da94a5e9adb51c852246
number23 <number23.cn@gmail.com>
2011-11-09 17:02:39 +0800

    CL: read-lines

--- Common-Lisp/ucl/ucl-01.lisp
+++ Common-Lisp/ucl/ucl-01.lisp
@@ -1,6 +1,16 @@
 (in-package #:ucl)
 
-(export '(print-list))
+(export '(print-list
+          read-lines))
 
 (defun print-list (lst)
     (dolist (x lst) (print x)))
+
+(defun read-lines (file)
+  (let (lines)
+    (with-open-file (stream file)
+      (do ((line (read-line stream nil)
+                 (read-line stream nil)))
+          ((null line))
+        (push line lines)))
+    (reverse lines)))
