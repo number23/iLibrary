e0d186eee8ca5e752df19871654e64e208a6afea
number23 <number23.cn@gmail.com>
2011-11-09 16:19:39 +0800

    CL: day-of-week

--- Common-Lisp/ucl/ucl-02.lisp
+++ Common-Lisp/ucl/ucl-02.lisp
@@ -19,3 +19,21 @@
 
 (defun get-unix-time ()
   (universal-to-unix-time (get-universal-time)))
+
+;;;; from http://cl-cookbook.sourceforge.net/dates_and_times.html
+(defun day-of-week (day month year)
+  "Returns the day of the week as an integer.
+Sunday is 0. Works for years after 1752."
+  (let ((offset '(0 3 2 5 0 3 5 1 4 6 2 4)))
+    (when (< month 3)
+      (decf year 1))
+    (mod
+     (truncate (+ year
+                  (/ year 4)
+                  (/ (- year)
+                     100)
+                  (/ year 400)
+                  (nth (1- month) offset)
+                  day
+                  -1))
+     7)))
