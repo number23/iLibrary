fb89a2a1d5ea8384a10cba809c07f3c34da28b47
number23 <number23.cn@gmail.com>
2011-11-25 23:00:15 +0800

    update .clojure.clj

--- clojure/.clojure.clj
+++ clojure/.clojure.clj
@@ -1,2 +1,7 @@
 (defn exit [] (System/exit 0))
 (defn quit [] (System/exit 0))
+(defmacro kw
+  "查询当前所有ns中含特定字符串的函数，如: (kw -index)
+  from http://www.newsmth.net/bbscon.php?bid=579&id=25982"
+  [s] `(filter #(>= (.indexOf (str %) (name '~s)) 0)
+               (sort (keys (mapcat ns-publics (all-ns))))))
