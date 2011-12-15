97b696578e0a52df88726b68710fcafbc5ce8afb
number23 <number23.cn@gmail.com>
2011-12-15 14:55:50 +0800

    update completions.clj

--- clojure/completions.clj
+++ clojure/completions.clj
@@ -1,5 +1,6 @@
 (use '(clojure pprint repl))
 (use '(clojure.java shell browse javadoc jdbc))
+(load-file (str (System/getenv "HOME") "/.clojure.clj"))
 
 (def completions (mapcat (comp keys ns-publics) (all-ns)))
 (with-open [f (java.io.BufferedWriter. (java.io.FileWriter. (str (System/getenv "HOME") "/.clj_completions")))]
