8cf1a62dd68048c2e8d0540a9a86332b1ec42af3
number23 <number23.cn@gmail.com>
2012-06-11 14:27:52 +0800

    .clojure.clj: add scaffold func

--- clojure/.clojure.clj
+++ clojure/.clojure.clj
@@ -11,3 +11,19 @@
 (defmacro unless [condition & body]
   `(if (not ~condition)
      (do ~@body)))
+
+;; from <<Clojure Programming>>, P292.
+(defn scaffold
+  "Given an interface, returns a 'hollow' body suitable for use with `deftype`."
+  [interface]
+  (doseq [[iface methods] (->> interface
+                               .getMethods
+                               (map #(vector (.getName (.getDeclaringClass %))
+                                             (symbol (.getName %))
+                                             (count (.getParameterTypes %))))
+                               (group-by first))]
+    (println (str " " iface))
+    (doseq [[_ name argcount] methods]
+      (println
+       (str "    "
+            (list name (into '[this] (take argcount (repeatedly gensym)))))))))
